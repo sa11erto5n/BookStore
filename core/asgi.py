dca157798b6175c5896c7e1eb79e742acb9b8817
sa11erto5n <amrmoh023@gmail.com>
2024-08-08 18:38:02 +0100

    init

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from decouple import config
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SERVER_ENV'))
 
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from decouple import config
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SERVER_ENV'))
 
 application = get_wsgi_application()
